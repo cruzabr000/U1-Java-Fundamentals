d4978effeed62ba4341ebd5ea50139662b0be40c
Cruzabr000 <cruzabr000@warren.k12.in.us>
2021-08-04 12:07:21 -0400

    Competed L1 and L2

--- src/L2.java
+++ src/L2.java
@@ -1,4 +1,24 @@
 public class L2 {
 
+    //COMMENTS
+
+    public static void main(String[] args){
+        String firstName = "George";
+        int socailSecurityNumber = 1234;
+        int age = 16;
+        System.out.println(firstName);
+        System.out.println(age);
+
+        String customerName = "George";
+        int accountID = 1256734;
+        double balance = 1203.96;
+
+        System.out.println(customerName);
+        System.out.println(accountID);
+        System.out.println(balance);
+        System.out.println(age);
+    }
+
+
     //add the main method header
 }
